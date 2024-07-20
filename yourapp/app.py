4589fd5949eaa35d7c7262e5a585bcd5e035daa1
vbwjsrh <vbwjsrh@naver.com>
2024-07-20 18:02:47 +0900

    FEAT: Output QutantStats report

--- yourapp/app.py
+++ yourapp/app.py
@@ -12,17 +12,19 @@ from scipy.stats import norm
 from io import BytesIO
 import base64
 import matplotlib
+import tempfile
+from PIL import Image
 
-matplotlib.use('Agg') #Engine reset issue solution code (TkAgg->Agg)
+matplotlib.use('Agg') # Engine reset issue solution code (TkAgg->Agg)
 
 load_dotenv()
 
-genai.configure(api_key = os.environ.get('GEMINI_API_KEY'))
+genai.configure(api_key=os.environ.get('GEMINI_API_KEY'))
 
 app = Flask(__name__)
 
 
-# Function to get ETF price data
+# Function to get ETF price data 
 def get_etf_price_data():
     tickers = ['AAPL', 'MSFT', 'NVDA', 'GOOG', 'XLK']
     etf = yf.Tickers(tickers)
@@ -74,7 +76,6 @@ class GEMTU772:
             bounds = ((0.0, 1.0), ) * noa
             weights_sum_to_1 = {'type': 'eq', 'fun': lambda weights: np.sum(weights) - 1}
 
-            
             def neg_sharpe(weights, er, cov):
                 r = weights.T @ er # @ means multiplication
                 vol = np.sqrt(weights.T @ cov @ weights)
@@ -147,52 +148,61 @@ class GEMTU772:
                 VaR = rets.quantile(delta)
                 return rets[rets <= VaR].mean()
 
-            rolling_CVaR = -port_rets.rolling(param).apply(calculate_CVaR, args=(delta,)).fillna(0)
+            rolling_CVaR = -port_rets.rolling(param).apply(calculate_CVaR, args=(delta,))
             weights = (cvar_target / rolling_CVaR).replace([np.inf, -np.inf], 0).shift(1).fillna(0)
             weights[weights > 1] = 1
             return weights
         #KL
         def kl(self, port_rets, param):
-            sharpe_ratio = (port_rets.rolling(param).mean() * np.sqrt(param) / port_rets.rolling(param).std())
-            weights = pd.Series(2 * norm.cdf(sharpe_ratio) - 1, index=port_rets.index).fillna(0)
-            weights[weights < 0] = 0
-            weights = weights.shift(1).fillna(0)
-            return weights
+            rolling_mean = port_rets.rolling(param).mean()
+            rolling_vol = port_rets.rolling(param).std()
+            kelly_weights = rolling_mean / rolling_vol**2
+            kelly_weights = kelly_weights.replace([np.inf, -np.inf], 0).shift(1).fillna(0)
+            kelly_weights[kelly_weights < 0] = 0
+            kelly_weights[kelly_weights > 1] = 1
+            return kelly_weights
         #CPPI
-        def cppi(self, port_rets, m=3, floor=0.7, init_val=1):
-            n_steps = len(port_rets)
-            port_value = init_val
-            floor_value = init_val * floor
-            peak = init_val
+        def cppi(self, port_rets, floor_value=1, cushion=0.1):
+            port_value = (1 + port_rets).cumprod()
+            weights = (port_value - floor_value) / port_value
+            weights = weights.clip(lower=0).shift(1).fillna(0)
+            return weights
 
-            port_history = pd.Series(dtype=np.float64).reindex_like(port_rets)
-            weight_history = pd.Series(dtype=np.float64).reindex_like(port_rets)
-            floor_history = pd.Series(dtype=np.float64).reindex_like(port_rets)
+    # Portfolio Value Calculation
+    def portfolio_value(self, cs_model, ts_model, cost=0.0005, initial_value=1):
+        weight_history = pd.DataFrame(index=self.rets.index, columns=self.rets.columns)
+        port_history = pd.Series(index=self.rets.index)
+        floor_history = pd.Series(index=self.rets.index)
 
-            for step in range(n_steps):
-                peak = np.maximum(peak, port_value)
-                floor_value = peak * floor
+        # Calculate Portfolio Values
+        port_value = initial_value
+        floor_value = initial_value
+        for step in self.rets.index[self.param-1:]:
+            # Get Weight and Portfolio Return
+            weight = self.run(cs_model, ts_model, cost)[0].loc[step]
+            port_rets = self.rets.loc[step]
+            risky_alloc = weight.sum()
+            safe_alloc = port_value - risky_alloc
 
-                cushion = (port_value - floor_value) / port_value
-                weight = m * cushion
+            # Calculate Portfolio Value
+            port_value = risky_alloc * (1 + port_rets) + safe_alloc
 
-                risky_alloc = port_value * weight
-                safe_alloc = port_value * (1 - weight)
-                port_value = risky_alloc * (1 + port_rets.iloc[step]) + safe_alloc
+            # Store Values
+            port_history.loc[step] = port_value
+            weight_history.loc[step] = weight
+            floor_history.loc[step] = floor_value
 
-                port_history.iloc[step] = port_value
-                weight_history.iloc[step] = weight
-                floor_history.iloc[step] = floor_value
-
-            return weight_history.shift(1).fillna(0)
+        return weight_history.shift(1).fillna(0)
    
     # Transaction Cost Function (Compound rate of return method assuming reinvestment)
     def transaction_cost(self, weights_df, rets_df, cost=0.0005):
-        prev_weights_df = (weights_df.shift(1).fillna(0) * (1 + rets_df.iloc[self.param-1:,:])) \
-            .div((weights_df.shift(1).fillna(0) * (1 + rets_df.iloc[self.param-1:,:])).sum(axis=1), axis=0)
-       
+        prev_weights_df = weights_df.shift(1).fillna(0) * (1 + rets_df.iloc[self.param-1:,:])
+        sum_weights = (weights_df.shift(1).fillna(0) * (1 + rets_df.iloc[self.param-1:,:])).sum(axis=1)
+        sum_weights_replaced = sum_weights.replace(0, np.nan)
+        normalized_weights_df = prev_weights_df.div(sum_weights_replaced, axis=0)
+        
         # Investment Weight of Previous Period (The backslash ('\') in Python is used as a line continuation character.)
-        cost_df = abs(weights_df - prev_weights_df) * cost
+        cost_df = abs(weights_df - normalized_weights_df) * cost
         cost_df.fillna(0, inplace=True)
         return cost_df
   
@@ -204,8 +214,8 @@ class GEMTU772:
         
         # Intraday Return Rate DataFrame
         rets = self.rets
-      
-        # Select and Run Cross-Sectional Risk Models
+    
+        # Select and Run Cross-Sectional Risk Models  
         for i, index in enumerate(rets.index[self.param-1:]):
             if cs_model == 'EW':
                 backtest_dict[index] = self.CrossSectional().ew(self.er[i])
@@ -219,17 +229,17 @@ class GEMTU772:
                 backtest_dict[index] = self.CrossSectional().emv(self.vol[i])
             elif cs_model == 'RP':
                 backtest_dict[index] = self.CrossSectional().rp(self.cov[i])
-     
-        # Cross-Sectional Weights DataFrame
+        
+        # Cross-Sectional Weights DataFrame    
         cs_weights = pd.DataFrame(list(backtest_dict.values()), index=backtest_dict.keys(), columns=rets.columns)   
         cs_weights.fillna(0, inplace=True)
         
-         # Cross-Sectional Risk Models Return on Assets
+        # Cross-Sectional Risk Models Return on Assets
         cs_rets = cs_weights.shift(1) * rets.iloc[self.param-1:,:]
-       
+        
         # Cross-Sectional Risk Models Portfolio Return
         cs_port_rets = cs_rets.sum(axis=1)
-        
+       
         # Select and Run Time-Series Risk Models
         if ts_model == 'VT':
             ts_weights = self.TimeSeries().vt(cs_port_rets, self.param)
@@ -242,7 +252,7 @@ class GEMTU772:
         elif ts_model == None:
             ts_weights = 1
             
-        # Final Portfolio Investment Weights
+        # Final Portfolio Investment Weights    
         port_weights = cs_weights.multiply(ts_weights, axis=0)
         
         # Transaction Cost DataFrame
@@ -256,52 +266,20 @@ class GEMTU772:
         port_rets.index = pd.to_datetime(port_rets.index).strftime("%Y-%m-%d")
 
         return port_weights, port_asset_rets, port_rets
-
-    def performance_analytics(self, port_weights, port_asset_rets, port_rets):
-            img = BytesIO()
-            
-            # Portfolio Weights Plot
-            plt.figure(figsize=(12, 7))
-            port_weights['Cash'] = 1 - port_weights.sum(axis=1)
-            plt.stackplot(port_weights.index, port_weights.T, labels=port_weights.columns)
-            plt.title('Portfolio Weights')
-            plt.xlabel('Date')
-            plt.ylabel('Weights')
-            plt.legend(loc='upper left')
-            plt.savefig(img, format='png')
-            plt.close()
-            img.seek(0)
-            plot_url1 = base64.b64encode(img.getvalue()).decode('utf8')
-
-            img = BytesIO()
-            # Underlying Asset Performance Plot
-            plt.figure(figsize=(12, 7))
-            plt.plot((1 + port_asset_rets).cumprod() - 1)
-            plt.title('Underlying Asset Performance')
-            plt.xlabel('Date')
-            plt.ylabel('Returns')
-            plt.legend(port_asset_rets.columns, loc='upper left')
-            plt.savefig(img, format='png')
-            plt.close()
-            img.seek(0)
-            plot_url2 = base64.b64encode(img.getvalue()).decode('utf8')
-
-            img = BytesIO()
-            # Portfolio Performance Plot
-            plt.figure(figsize=(12, 7))
-            plt.plot((1 + port_rets).cumprod() - 1)
-            plt.title('Portfolio Performance')
-            plt.xlabel('Date')
-            plt.ylabel('Returns')
-            plt.savefig(img, format='png')
-            plt.close()
-            img.seek(0)
-            plot_url3 = base64.b64encode(img.getvalue()).decode('utf8')
-
-            return plot_url1, plot_url2, plot_url3
-    
     
 
+    def performance_analytics(self, port_rets):
+        if not isinstance(port_rets.index, pd.DatetimeIndex):
+            port_rets.index = pd.to_datetime(port_rets.index)
+
+        with tempfile.NamedTemporaryFile(delete=False, suffix='.html') as tmp_file:
+            qs.reports.html(port_rets, output=tmp_file.name)
+            tmp_file.seek(0)
+            report_html = tmp_file.read().decode('utf-8')
+
+        return report_html
+    
+    
 # Create the model
 # See https://ai.google.dev/api/python/google/generativeai/GenerativeModel
 generation_config = {
@@ -331,23 +309,24 @@ safety_settings = [
     }
 ]
     
-
 #Route function
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    if request.method == 'POST':
-        cs_model = request.form.get('cs_model')  # cs model selection
-        ts_model = request.form.get('ts_model')  # ts model selection
-        engine = GEMTU772(df)  # Run backtesting
-        res = engine.run(cs_model=cs_model, ts_model=ts_model, cost=0.0005)  # run method
-        port_weights = res[0]
-        port_asset_rets = res[1]
-        port_rets = res[2]
-        plot_url1, plot_url2, plot_url3 = engine.performance_analytics(port_weights, port_asset_rets, port_rets)
-        return render_template('index.html', plot_url1=plot_url1, plot_url2=plot_url2, plot_url3=plot_url3)  # Rendering
+@app.route('/', methods=["GET", "POST"])
+def report():
+    if request.method == "POST":
+        cs_model = request.form.get('cs_model') # cs model selection
+        ts_model = request.form.get('ts_model') # ts model selection
+        engine = GEMTU772(df) # Run backtesting
+        res = engine.run(cs_model=cs_model, ts_model=ts_model, cost=0.0005)
+        port_weights, port_asset_rets, port_rets = res
+
+        report_html = engine.performance_analytics(port_rets)
+
+        return render_template('report_viewer.html', report_html=report_html)
+
     return render_template('index.html')
 
-    
+
+
 @app.route("/generate_text", methods=["GET", "POST"])
 def generate_text():
     if request.method == "POST":
@@ -374,23 +353,30 @@ def generate_text():
             },
             "data": None
         }), 405
-
+        
+def process_image_data(img_data):
+    img_bytes = base64.b64decode(img_data)
+    img = Image.open(BytesIO(img_bytes))
+    return img
 
 @app.route("/generate_text_stream", methods=["GET", "POST"])
 def generate_text_stream():
     if request.method == "POST":
         input_data = request.get_json()
         prompt = input_data["prompt"]
+        image_data = input_data.get("images", [])
+        images = [process_image_data(img) for img in image_data]
         model = genai.GenerativeModel(model_name="gemini-1.5-pro",
                                       generation_config=generation_config,
                                       safety_settings=safety_settings)
 
         def generate_stream():
-            response = model.generate_content(prompt, stream=True)
+            response = model.generate_content([prompt] + images, stream=True)
             for chunk in response:
                 print(chunk.text)
                 yield chunk.text + "\n"
-
+                
+                
         return Response(stream_with_context(generate_stream()), mimetype="text/plain")
     else:
         return jsonify({
@@ -401,7 +387,5 @@ def generate_text_stream():
             "data": None
         }), 405
 
-
-
 if __name__ == '__main__':
-    app.run(debug=True, host="127.0.0.1", port=int(os.environ.get("PORT", 5000)))
+    app.run(debug=True, host="127.0.0.1", port=int(os.environ.get("PORT", 5000))) 
